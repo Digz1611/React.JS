bfd0954a95707aa7b88697c222588f8319e12142
Digz1611 <diego.langeveldt@gmail.com>
2024-05-06 12:02:13 +0200

    5,6,7 done

--- Chapter_7/my_first_react_app/src/App.js
+++ Chapter_7/my_first_react_app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Products from './Products';
+import GitHub from './GitHub';
 
-class App extends Component {
+class App extends Component { 
+      
   render() {        
     return (
       <div>
-        <Products />        
+        <GitHub />                        
       </div>
     );
   }
